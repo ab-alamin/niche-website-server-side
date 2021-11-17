565217ae807572451e7f4cfb80d4ae018e7d251a
ab-alamin <abdullahalamin9876@gmail.com>
2021-11-17 15:48:23 +0600

    update

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const bodyParser = require("body-parser");
 require('dotenv').config();
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient } = require('mongodb').MongoClient;
 const ObjectId =  require('mongodb').ObjectId;
 
-const app = express();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djm8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true,
+     }); 
 const port = process.env.PORT || 5000;
+const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djm8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-console.log(uri);
-
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); 
-
-async function run() {
-    try{
-        await client.connect();
+app.get('/', (req, res) => {
+    res.send(' motors Website');
+});
+client.connect( (err) => {
         const database = client.db("motors_portal");
         const servicesCollection = database.collection('services');
+        const confirmApplyCollection = client
+    .db("motors_portal")
+    .collection("confirmApply");
+  const reviewCollection = client
+    .db("motors_portal")
+    .collection("review");
+  const adminCollection = client
+    .db("motors_portal")
+    .collection("admin");
 
         // GET API 
         app.get('/services', async(req, res) => {
             const cursor = servicesCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
+            const services = await cursor.toArray((err, documents) => {
+                res.send(documents);
+              });
         });
         
 
@@ -40,7 +53,7 @@ async function run() {
             const query = {_id:ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.json(service);
-        })
+        });
 
         // POST API
         app.post('/services', async(req, res) =>{
@@ -51,6 +64,35 @@ async function run() {
         res.json(result);
 
         });
+        app.post("/addService", (req, res) => {
+            console.log(req.body);
+            universityCollection.insertOne(req.body).then((result) => {
+              res.send(result.insertedCount > 0);
+              console.log(result.insertedCount > 0);
+            });
+          });
+
+        pp.post("/addReview", (req, res) => {
+            console.log(req.body);
+            reviewCollection.insertOne(req.body).then((result) => {
+              res.send(result.insertedCount > 0);
+              console.log(result.insertedCount > 0);
+            });
+          });
+
+          app.get("/ClientReview", (req, res) => {
+            reviewCollection.find({}).toArray((err, documents) => {
+              res.send(documents);
+            });
+          });
+
+          app.post("/addAdmin", (req, res) => {
+            adminCollection.insertOne(req.body).then((result) => {
+              console.log(result.insertedCount > 0);
+              res.send(result.insertedCount > 0);
+            });
+          });
+        
 
         // Delete API
         app.delete('/services/:id', async(req, res) => {
@@ -59,19 +101,61 @@ async function run() {
             const result = await servicesCollection.deleteOne(query);
             res.json(result);
         })
-    }
-    finally {
-        // await client.close();
-      }
 
-}
-run().catch(console.dir);
+        app.get("/allOrders", (req, res) => {
+            confirmApplyCollection.find({}).toArray((err, documents) => {
+              res.send(documents);
+            });
+          });
 
-app.get('/', (req, res) => {
-    res.send(' travelling Website');
-});
-
-
-app.listen(port, () => {
-    console.log('running server on port: ' , port);
-});  
+          app.get("/checkAdmin", (req, res) => {
+            adminCollection
+              .find({ email: req.query.email })
+              .toArray((err, documents) => {
+                console.log(documents.length > 0);
+                res.send(documents.length > 0);
+              });
+          });
+          app.patch("/updateStatus/:id", (req, res) => {
+            console.log(req.body.optionValue);
+            confirmApplyCollection
+              .updateOne(
+                { _id: ObjectId(req.params.id) },
+                {
+                  $set: { process: req.body.optionValue },
+                }
+              )
+              .then((result) => {
+                console.log(result.modifiedCount > 0);
+                res.send(result.modifiedCount > 0);
+              });
+          });
+          app.patch("/update/:id", (req, res) => {
+            console.log(req.body.name);
+            servicesCollection
+              .updateOne(
+                { _id: ObjectId(req.params.id) },
+                {
+                  $set: {
+                    name: req.body.name,
+                    location: req.body.name,
+                    serviceCharge: req.body.serviceCharge,
+                    type: req.body.type,
+                  },
+                }
+              )
+              .then((result) => {
+                res.send(result.modifiedCount > 0);
+                console.log(result.modifiedCount > 0);
+              });
+          });
+          app.get("/serviceUpdate/:id", (req, res) => {
+            console.log(req.params.id);
+            servicesCollection
+              .find({ _id: ObjectId(req.params.id) })
+              .toArray((err, documents) => {
+                res.send(documents[0]);
+              });
+          });
+       });
+app.listen(process.env.PORT || port);
